1bf8836cb979f8f53fbc48441cf3e269c30c8ee6
Eadweard <aurora.coder.zqf@gmail.com>
2024-08-12 10:12:03 +0800

    eslint fix

--- app/components/transition-area/TransitionInput.vue
+++ app/components/transition-area/TransitionInput.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
-import { addStyle, clearStyle, handleSelectLanguage, translateFromOptions, useTranslateFromSelect} from "~/composables/useTranslateFrom";
+import {
+  addStyle,
+  clearStyle,
+  handleSelectLanguage,
+  translateFromOptions,
+  useTranslateFromSelect
+} from "~/composables/useTranslateFrom";
 import {debounce} from 'lodash';
 
 const languageSelected = useTranslateFromSelect();
@@ -19,7 +25,8 @@ const handlerGetText = debounce(getText, 500, {leading: false, trailing: true});
 </script>
 
 <template>
-  <div id="translate_from_area" class="transition_input inline-flex flex-col" @mouseenter="addStyle" @mouseleave="clearStyle">
+  <div id="translate_from_area" class="transition_input inline-flex flex-col" @mouseenter="addStyle"
+       @mouseleave="clearStyle">
     <header class="w-full h-[40px] cursor-pointer inline-flex items-center">
       <n-dropdown
           trigger="hover"
@@ -27,17 +34,19 @@ const handlerGetText = debounce(getText, 500, {leading: false, trailing: true});
           :options="translateFromOptions"
           @select="handleSelectLanguage"
       >
-      <div class="inline-flex h-[22px] px-[2px] py-[2px] cursor-pointer hover:bg-gray-100 from_language rounded" @click="switchExpandSelectLanguage">
-        <span>{{ languageSelected.text }}</span>
-        <img v-if="!isShowSelectPanel" class="w-[16px] h-[16px]" src="https://cdn-web.caiyunapp.com/lingoCloud/newVersion/img/downArrow.png" alt="">
-        <img v-else class="w-[16px] h-[16px]" src="https://cdn-web.caiyunapp.com/lingoCloud/newVersion/img/downArrow.png" alt="">
-      </div>
+        <div class="inline-flex h-[22px] px-[2px] py-[2px] cursor-pointer hover:bg-gray-100 from_language rounded"
+             @click="switchExpandSelectLanguage">
+          <span>{{ languageSelected.text }}</span>
+          <img v-if="!isShowSelectPanel" class="w-[16px] h-[16px]"
+               src="https://cdn-web.caiyunapp.com/lingoCloud/newVersion/img/downArrow.png" alt="">
+          <img v-else class="w-[16px] h-[16px]"
+               src="https://cdn-web.caiyunapp.com/lingoCloud/newVersion/img/downArrow.png" alt="">
+        </div>
       </n-dropdown>
-
-      <img class="w-[16px] h-[16px] ml-[5px]" src="https://cdn-web.caiyunapp.com/lingoCloud/newVersion/img/change.png" alt="">
+      <img class="w-[16px] h-[16px] ml-[5px]" src="https://cdn-web.caiyunapp.com/lingoCloud/newVersion/img/change.png"
+           alt="">
     </header>
-    <textarea placeholder="请输入要翻译的文字"  @input="handlerGetText"></textarea>
-    
+    <textarea placeholder="请输入要翻译的文字" @input="handlerGetText"></textarea>
   </div>
 </template>
 
@@ -52,7 +61,7 @@ const handlerGetText = debounce(getText, 500, {leading: false, trailing: true});
   color: #333;
   border-radius: 5px;
   box-sizing: border-box;
-  
+
   textarea {
     min-height: 240px;
     cursor: pointer;
@@ -65,24 +74,27 @@ const handlerGetText = debounce(getText, 500, {leading: false, trailing: true});
     font-weight: 500;
     line-height: 26px;
     overflow: auto;
+
     &:focus {
       //outline: none;
     }
+
     &:focus-visible {
       outline: none;
     }
   }
-  
+
   header {
     .from_language {
       vertical-align: center;
     }
+
     span {
       color: #000000e0;
       font-size: 13px;
       font-weight: 500;
       vertical-align: middle;
-      font-family: PingFang SC,sans-serif;
+      font-family: PingFang SC, sans-serif;
     }
   }
 }

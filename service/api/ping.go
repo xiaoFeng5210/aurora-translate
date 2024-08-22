fa759473d8d90dc2270b21bac2182adc8553e9bf
Eadweard <aurora.coder.zqf@gmail.com>
2024-08-22 17:56:45 +0800

    web

--- service/api/ping.go
+++ service/api/ping.go
@@ -1,9 +1,18 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Ping(c *gin.Context) {
+	test := []string{"test1", "test2"}
+	for i := 0; i < len(test); i++ {
+		fmt.Println(test[i])
+	}
+
 	c.JSON(200, gin.H{
-		"message": "pong",
+		"message": test,
 	})
 }
--- service/api/translateText.go
+++ service/api/translateText.go
@@ -1,6 +1,12 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 type TranslationRequest struct {
 	Source    []string `json:"source"`
@@ -9,8 +15,24 @@ type TranslationRequest struct {
 
 func TranslateText(c *gin.Context) {
 	var req TranslationRequest
+
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	// 把拿到的req写到文件中
+	// 指定文件名
+	filename := "test.json"
+
+	jsonData, err := json.Marshal(req)
+	if err != nil {
+		fmt.Println("json.Marshal failed, err:", err)
+		return
+	}
+
+	err = os.WriteFile(filename, jsonData, 0644)
+	if err != nil {
+		fmt.Println("write file failed, err:", err)
 		return
 	}
 

14a54974a4242ee956ea666e975ca29ee2aecb33
Eadweard <aurora.coder.zqf@gmail.com>
2024-09-10 15:39:09 +0800

    finish

--- service/api/ping.go
+++ service/api/ping.go
@@ -1,16 +1,13 @@
 package api
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Ping(c *gin.Context) {
 	test := []string{"test1", "test2"}
-	for i := 0; i < len(test); i++ {
-		fmt.Println(test[i])
-	}
+
+	// ... existing code ...
 
 	c.JSON(200, gin.H{
 		"message": test,

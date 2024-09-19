b8a6579e26f44a54172041fdedc8554a8aed89b8
Eadweard <aurora.coder.zqf@gmail.com>
2024-09-19 14:16:51 +0800

    处理和优化

--- app/composables/useFetchTranslate.ts
+++ app/composables/useFetchTranslate.ts
@@ -1,16 +1,16 @@
-import { useTranslateFromSelect } from './useTranslateFromSelect'
+import { useTranslateFromSelect } from './useTranslateFrom'
 
 function useFetchTranslate() {
   const languageSelected = useTranslateFromSelect()
 
   const fetchTranslateText = async (textContent: string) => {
-    const data = {
+    const body = {
       source: [textContent],
       direction: 'auto2zh',
     }
 
     const { data, status, error, refresh, clear } = await useFetch('api/translate/text', {
-      body: data,
+      body,
     })
   }
 

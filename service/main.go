1ff64306cd17e776627bd36c850e32f2e636b0ea
Eadweard <aurora.coder.zqf@gmail.com>
2024-12-09 17:12:55 +0800

    gorm尝试链接postgres

--- service/main.go
+++ service/main.go
@@ -6,13 +6,27 @@ import (
 	"translate-api/utils"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+
 	logger := utils.GetLogger()
 	logger.Info("服务正在启动......")
 
 	r := gin.Default()
 	config.Routes(r)
 	r.Run("0.0.0.0:8080") // listen and serve on 0.0.0.0:8080
+}
+
+func dbConnect() {
+	logger := utils.GetLogger()
+	dsn := "host=localhost user=xiaofeng password=12345678 dbname=translate-aurora port=5431 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		logger.Error("连接数据库失败！", zap.Error(err))
+	}
+	logger.Info("连接数据库成功")
 }

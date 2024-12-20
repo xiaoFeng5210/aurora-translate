c978827e778cf0f0a9f1f8ee615bcb2b61ab927b
Eadweard <aurora.coder.zqf@gmail.com>
2024-12-20 13:48:12 +0800

    fix bug

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
       },
     },
   },
-  compatibilityDate: '2024-04-03', e
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['nuxtjs-naive-ui'],
   vite: {

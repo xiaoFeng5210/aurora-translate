8d661e338c4868ddd943aa083a9b98cd0d5854ed
Eadweard <aurora.coder.zqf@gmail.com>
2025-01-03 14:10:32 +0800

    创建用户前检查

--- service/api/users.go
+++ service/api/users.go
@@ -10,6 +10,17 @@ import (
 	"go.uber.org/zap"
 )
 
+func CheckUserExists(username string) bool {
+	var user dto.User
+	db := db.GetDB()
+	result := db.Where("username = ?", username).First(&user)
+	if result.Error != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
 // 创建用户
 func CreateUser(c *gin.Context) {
 	logger := utils.GetLogger()
@@ -21,6 +32,15 @@ func CreateUser(c *gin.Context) {
 			"code":     -1,
 			"message":  "解析创建用户请求参数失败",
 			"errorMsg": err.Error(),
+		})
+		return
+	}
+
+	if CheckUserExists(req.Username) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":     -1,
+			"message":  "用户已存在",
+			"errorMsg": "用户已存在",
 		})
 		return
 	}

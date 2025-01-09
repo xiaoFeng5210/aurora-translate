78fb69b30122ccb81a314b732b8453a5aeb9f50c
Eadweard <aurora.coder.zqf@gmail.com>
2025-01-09 15:56:48 +0800

    去掉console

--- app/utils/help.ts
+++ app/utils/help.ts
@@ -2,7 +2,6 @@
  * 处理语言
  */
 export function language2params(language: { language: string, text: string }) {
-  console.log(' language', language)
   if (language.language === 'zh_CN') {
     return 'auto2zh'
   }

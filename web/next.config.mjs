99f6891d3dd4a50513ee7f273a1eb045ce991c54
Eadweard <aurora.coder.zqf@gmail.com>
2025-02-24 11:48:10 +0800

    strickmode change false

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,7 @@
 import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: false,
     output: 'standalone',
     async rewrites() {
         return [

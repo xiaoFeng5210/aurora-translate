8161f1d81d934c02c765e37fa9c54cb1fa45d5ec
Eadweard <aurora.coder.zqf@gmail.com>
2025-03-12 10:38:28 +0800

    冒泡排序

--- service/utils/help.go
+++ service/utils/help.go
@@ -80,3 +80,16 @@ func LogApiRecord(path string, method int) {
 	logger := GetLogger()
 	logger.Info(fmt.Sprintf("%s, 路径：%s", methodStr, path))
 }
+
+// 冒泡排序
+func bubbleSort(arr []int) []int {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
